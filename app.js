5bc3478eb3a246130a0d7fa0e0bbfa0fdcb2edcf
r3Fuze <r3Fuze@gmail.com>
2013-07-20 16:52:31 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,66 @@
+/* TODO:
+    - Install other dependencies (socket.io, etc)
+ * ====================== */
+
+/* Dependencies
+ * ====================== */
+var express = require("express"),
+    http    = require("http"),
+
+    routes  = require("./routes/routes");
+
+
+var app = express();
+
+app.locals({
+    theme: "cosmo"
+});
+
+/* Configuration
+    - Set the port to 3000
+    - Set the views folder to /views
+    - Set the view engine to jade
+    - Enable case sensitive routing
+
+    - Parse request bodies
+    - Something with HTTP requests??
+    - Handle custom files before exposing /public??
+
+    - Handle .less stylesheets
+
+    - Serve static files??
+ * ====================== */
+app.configure(function() {
+    app.set("port", process.env.PORT || 3000);
+    app.set("views", __dirname + "/views");
+    app.set("view engine", "jade");
+    app.enable("case sensitive routing");
+
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+
+    app.use(require("less-middleware")({
+        dest: __dirname + '/public/css',
+        src: __dirname + '/public/less',
+        compress: true
+    }));
+
+    app.use(express.static(__dirname + "/public"));
+});
+
+
+/* Routes
+ * ====================== */
+app.get("/", function(req, res) {
+    res.render("index", {
+        title: "Building Web Apps in Node with Express"
+    });
+});
+
+
+/* Create the server
+ * ====================== */
+http.createServer(app).listen(app.get("port"), function() {
+    console.log("Express server listening on port " + app.get("port"));
+});
